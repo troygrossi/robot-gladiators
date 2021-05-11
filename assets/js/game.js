503bb2d7451b56ec557c10c165e3dd195d0f7b87
Troy Grossi <ttroygrossit@gmail.com>
2021-05-11 12:30:04 -0700

    added total of 3 enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,12 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+var fight = function (enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
   var promptFight = window.prompt(
@@ -68,9 +66,13 @@ var fight = function () {
     }
     // if no (false), ask question again by running fight() again
     else {
-      fight();
+      for (var i = 0; i < enemyNames.length; i++) {
+        fight(enemyNames[i]);
+      }
     }
   }
 };
 
-fight();
+for (var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
